efda8c2a297c865b3ad7db7a85044261d675588b
Dotsworthy <dotsworthy@gmail.com>
2020-06-29 14:20:14 +0100

    moved css to scss file

--- src/components/header.js
+++ src/components/header.js
@@ -3,46 +3,14 @@ import PropTypes from "prop-types"
 import React from "react"
 
 const Header = ({ siteTitle }) => (
-  <header
-    style={{
-      background: `#216869`,
-      // marginBottom: `1.45rem`,
-      height: '50px',
-      width: '100%',
-    }}
-  >
-    <div
-      style={{
-        margin: `0px`,
-        width: '100%',
-        height: '100%',
-        // padding: `0 25px`,
-        display: 'flex',
-        alignItems: 'center',
-        justifyContent: 'space-evenly',
-      }}
-    >
-      <h3 style={{ margin: 0 }}>
-        <Link
-          to="/"
-          style={{
-            color: `white`,
-            textDecoration: `none`,
-          }}
-        >
-          {siteTitle}
-        </Link>
-      </h3>
-      <div
-      style={{
-        display: 'flex',
-        width: '25%',
-        justifyContent: 'space-evenly'
-      }}>
+  <header>
+    <div className='navbar'>
       <a href='#about'>About</a>
+      <a href='#cv'>CV</a>
       <a href='#showreel'>Showreel</a>
+      <a href='#contact'>Contact</a>
+      <a href="mailto:adam.r.harper@gmail.com">adam.r.harper@gmail.com</a>
       </div>
-    </div>
   </header>
 )
 
--- src/pages/index.js
+++ src/pages/index.js
@@ -9,38 +9,76 @@ import showreel from "../public/showreel.mp4"
 const IndexPage = () => (
   <Layout>
     <SEO title="Home" />
-    <div style={{ width: '100%', height: '563px' }}>
-      <div style={{width: '100%', height: '562px', position: 'absolute'}}>
-        <video 
-        width="100%" 
-        height="562px" 
-        autoPlay 
-        loop 
-        muted>
-        <source src={showreel} type="video/mp4"/>
+    <div className="title-container">
+      <div className="title-video">
+        <video autoPlay loop muted>
+          <source src={showreel} type="video/mp4"/>
         </video>
       </div>
 
-      <div style={{height: '563px', width:'100%', backgroundColor: '#49A078', opacity: '0.8', position: 'absolute'}}>
+      <div className="title-overlay">
       </div>
 
-      <div style={{display: 'flex', height: '562px', width: '100%', position: 'absolute', alignItems: 'center', justifyContent: 'center'}}>
-        <h1 style={{fontSize: '4rem', color: 'white'}}>Adam Harper: Editor</h1>
+      <div className="title-text">
+        <h1>Adam Harper: Editor</h1>
       </div>
      </div>
-  <div id="about" style={{display: 'flex', flexDirection: 'column', alignItems: 'center', justifyContent: 'center', height: '600px'}}>
+  <div id="about" className="content-container">
     <h1>About Me</h1>
-    <p>I am an editor in Edinburgh</p>
-    <p>Look at my CV!</p>
-    <p> Here is some more stuff</p>
+    <p>I am an experienced Edit Assistant/Runner who has worked on projects for various production companies and broadcasters including Ecosse Films and the BBC. I possess a good working knowledge of Avid Media Composer and Premiere Pro. PC & Mac Literate.</p>
   </div>
-  <div id="showreel"style={{display: 'flex', flexDirection: 'column', alignItems: 'center', justifyContent: 'center', height: '600px'}}>
+  <div id="cv" className="content-container">
+    <h1>CV</h1>
+    <h2>Credits</h2>
+
+    <p>Russian Data (Sept 2019)</p>
+    <p>BBC The Social</p>
+    <p>Edit Assistant. Comedy sketch released on social media</p>
+
+    <p>Homeless World Cup 2019. (July 2019)</p>
+    <p>QTV Sports</p>
+    <p>Edit Assistant. Online broadcast of the 2019 Homeless World Cup taking place in Cardiff.</p>
+
+    <p>Freedom Fields (Jun – Dec 2018)</p>
+    <p>Scottish Documentary Institute</p>
+    <p>Technical Assistant. Feature length documentary, 97 minutes, released worldwide on the film festival circuit.  </p>
+
+    <p>Wild Rose (Oct 2017)</p>
+    <p>Film4, BFI, Creative Scotland</p>
+    <p>Runner. Feature length drama, 101 minutes, theatrical release worldwide.</p>
+
+    <p>Hampstead (July 2016)</p>
+    <p>Ecosse Films.</p>
+    <p>Runner. Feature length drama, 102 minutes, theatrical release worldwide. </p>
+
+    <p>Wild Rose (Oct 2017)</p>
+    <p>Film4, BFI, Creative Scotland</p>
+    <p>Runner. Feature length drama, 101 minutes, theatrical release worldwide.</p>
+
+    <p>War and Peace (July 2015)</p>
+    <p>BBC Studios/BBC One</p>
+    <p>Runner. 6x60’ drama miniseries, television and online release. </p>
+
+    <p>Scottish Documentary Institute (Oct 2017 – Dec 2018).</p>
+    <p>Post-Production Technician.</p>
+    <p>In charge of the technical aspects of post-production and distribution of films. Main responsibilities include Assistant Editing, creating DCPs, film distribution, the filming and editing of masterclasses. </p>
+
+    <h2>Education</h2>
+    <p>2013 – 2014: MA in Filmmaking. Edinburgh Napier University, Edinburgh.</p>
+
+  </div>
+  <div id="showreel" className="content-container">
   <h1>Showreel</h1>
-  <video controls width="50%" height="auto">
+  <video controls>
         <source src={showreel} type="video/mp4"/>
         </video>
   </div>
-    <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
+  <div id="contact" className="content-container">
+    <h1>Contact</h1>
+    <p>email: adam.r.harper@gmail.com</p>
+    <p>phone: 07847 841 171</p>
+  </div>
+    <div>
     </div>
   </Layout>
 )
